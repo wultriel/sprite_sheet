046282fbb42e38fbb71642b2bb6cf06bec3e60c5
wultriel <wultriel@users.noreply.github.com>
2025-06-09 08:34:00 +0000

    deploy: 08b7264af4945f711e97a1f5d2095db6fc799b4b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "8191e843020c832c9cf8852a4b909d4c",
 "canvaskit/chromium/canvaskit.wasm": "f504de372e31c8031018a9ec0a9ef5f0",
 "canvaskit/chromium/canvaskit.js.symbols": "b61b5f4673c9698029fa0a746a9ad581",
-"flutter_bootstrap.js": "c40076c404029032747dc3b376e6e86f",
+"flutter_bootstrap.js": "91920248040ad00885b740a2eadb7ab9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "2a3f8fb4fbb44c97e57c0e16a5d69aa0",
 "/": "2a3f8fb4fbb44c97e57c0e16a5d69aa0",
